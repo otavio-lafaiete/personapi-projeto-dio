76371feafa72441d23072c6b6fb06dda40b4ad4f
otavio-lafaiete <lafayetteloup@gmail.com>
2021-07-22 15:09:10 -0300

    modificado classe PersonController

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -5,6 +5,7 @@ import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public MessageResponseDTO createPerson(Person person){
+    public MessageResponseDTO createPerson(@RequestBody Person person){
         Person savedPerson = personRepository.save(person);
         return MessageResponseDTO.builder()
                 .message("Created Person With ID: " + savedPerson.getId())
